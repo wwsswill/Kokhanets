5de3d1f66d5d85ab2dd615710d1b75afce0d38ef
wwsswill <154557440+wwsswill@users.noreply.github.com>
2024-01-12 10:22:40 +1200

    12.01.24
    Кл

---
+++ Задача №6.py
@@ -0,0 +1,19 @@
+a, b, v = [int(i) for i in input('Введите возраст мальчиков:').split()]
+print(f'Возраст Антона: {a}')
+print(f'Возраст Бориса: {b}')
+print(f'Возраст Виктора: {v}')
+if a > b and a > v:
+    print('Антон старше всех')
+elif b > a and b > v:
+    print('Борис старше всех')
+elif v > a and v > b:
+    print('Виктор старше всех')
+elif a == b and b > v and a > v:
+    print(f'Антон и Борис старше Виктра')
+elif a == v and v > b and a > b:
+    print(f'Антон и виктор старше Бориса')
+elif a == b and a == v and b == v and b == a and v == a and v == b:
+    print(f'мальчик одного возраста')
+else:
+    print(f'Борис и Виктор старше Антона')
+    
---
+++ Задача №7.py
@@ -0,0 +1,6 @@
+t = float(input)
+if t % 8 < 3:
+    color = 'Зелённый'
+else:
+    color = 'Красный'
+print('Сигнал для пешехода горт', color)
