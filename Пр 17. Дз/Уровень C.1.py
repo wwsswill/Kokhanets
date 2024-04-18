f85a0deaf2eb39106c131d102aab694d101096e6
wwsswill <vladimer5552@gmail.com>
2024-04-19 10:04:06 +1200

    Дз

---
+++ Пр 17. Дз/Уровень C.1.py
@@ -0,0 +1,7 @@
+stroka = input("Введите строку:\n")
+cho_menaem = input("Что меняем: ")
+chem_menaem = input("Чем заменить: ")
+
+izmen_stroka = stroka.replace(cho_menaem, chem_menaem)
+print("Результат")
+print(izmen_stroka)
